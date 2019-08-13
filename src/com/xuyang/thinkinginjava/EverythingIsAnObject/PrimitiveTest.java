334cc95acfb775fdd4010af5a7e5bf55f1196874
xuyang-happy <958607524@qq.com>
2019-08-14 00:36:33 +0800

    Exericise 1, page 89

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/PrimitiveTest.java
@@ -0,0 +1,15 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/14 0:32
+ * Exericise 1, page 89
+ */
+public class PrimitiveTest {
+    static int i;
+    static char c;
+    public static void main(String[] args) {
+        System.out.println("int = " + i);
+        System.out.println("char = " + c);
+    }
+}
