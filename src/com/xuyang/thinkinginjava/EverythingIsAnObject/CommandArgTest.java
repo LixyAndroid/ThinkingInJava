886a00ac18ceb4c7ae52fd8db08e38428598cb6d
xuyang-happy <958607524@qq.com>
2019-08-16 12:07:22 +0800

    Exercise 10, page 90

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/CommandArgTest.java
@@ -0,0 +1,14 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/16 12:06
+ * Exercise 10, page 90
+ */
+public class CommandArgTest {
+    public static void main(String[] args) {
+        System.out.println("args[0] = " + args[0]);
+        System.out.println("args[1] = " + args[1]);
+        System.out.println("args[2] = " + args[2]);
+    }
+}
