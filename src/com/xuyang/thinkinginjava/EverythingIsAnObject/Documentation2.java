3ab3d103fdf6a1491ffacc99214e5b8ca7881639
xuyang-happy <958607524@qq.com>
2019-08-16 12:20:46 +0800

    Exercise 13 - 2

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/Documentation2.java
@@ -0,0 +1,17 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+import java.util.Date;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/16 12:19
+ * Exercise 13 - 2
+ * System.out.println(new Date());
+ */
+public class Documentation2 {
+
+    Date d = new Date();
+    void showDate() {
+        System.out.println("Date = " + d);
+    }
+}
