092ff7d1f0bfc2ba51809c82b51c11f5d7f75d47
xuyang-happy <958607524@qq.com>
2019-08-16 12:19:11 +0800

    Exercise 13 - 1

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/Documentation1.java
@@ -0,0 +1,13 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/16 12:16
+ * Exercise 13 - 1
+ */
+public class Documentation1 {
+    /** A field comment */
+    public int i;
+    /** A method comment */
+    public void f() {}
+}
