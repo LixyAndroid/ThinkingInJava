a850e70ec98db77e65b2af6fb8ff9a333af7e828
xuyang-happy <958607524@qq.com>
2019-08-16 11:49:13 +0800

    Exericise 2, page 89

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/16 11:47
+ * Exericise 2, page 89
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
