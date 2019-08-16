e3d4a5957009f272d7d2b220af609c1fd10fc879
xuyang-happy <958607524@qq.com>
2019-08-16 12:21:35 +0800

    Exercise 13 - 3

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/Documentation3.java
@@ -0,0 +1,17 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+import java.util.Date;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/16 12:21
+ *  Exercise 13 - 3
+ *  You can even insert a list:
+ */
+public class Documentation3 {
+
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println("d = " + d);
+    }
+}
