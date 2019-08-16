1ee56806ea0ae3a947895fabaee354bb368392e7
xuyang-happy <958607524@qq.com>
2019-08-16 12:24:44 +0800

    Exercies 15, page 91

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/HelloDocTest.java
@@ -0,0 +1,15 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/16 12:23
+ * Exercies 15, page 91
+ */
+public class HelloDocTest {
+    /** main method executed by java
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
