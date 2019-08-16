b538c47046dbefffe8c8f9de836671aace3c29cd
xuyang-happy <958607524@qq.com>
2019-08-16 12:15:35 +0800

    Exercise 12, page 90

---
+++ src/com/xuyang/thinkinginjava/EverythingIsAnObject/DocTest.java
@@ -0,0 +1,22 @@
+package com.xuyang.thinkinginjava.EverythingIsAnObject;
+
+import java.util.Date;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/16 12:09
+ * Exercise 12, page 90
+ */
+public class DocTest {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
+/* Output: (55% match)
+
+ */
