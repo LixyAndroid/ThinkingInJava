39fff70ce348bb0821fbe6a772574035f168fe4f
xuyang-happy <958607524@qq.com>
2019-08-26 09:57:21 +0800

    Exercise 2, page 97   这里需要net.mindview.util.Print,有需要自行去下载

---
+++ src/com/xuyang/thinkinginjava/Operators/Assign.java
@@ -0,0 +1,26 @@
+package com.xuyang.thinkinginjava.Operators;
+
+//import org.greggordon.tools.*;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/26 9:54
+ * Exercise 2, page 97
+ */
+
+class Tube {
+    float level;
+}
+public class Assign {
+    public static void main(String[] args) {
+        Tube t1 = new Tube();
+        Tube t2 = new Tube();
+        t1.level = 0.9f;
+        t2.level = 0.47f;
+//        P.rintln("1: t1.level: " + t1.level + ", t2.level: " + t2.level);
+//        t1 = t2;
+//        P.rintln("2: t1.level: " + t1.level + ", t2.level: " + t2.level);
+//        t1.level = 0.27f;
+//        P.rintln("3: t1.level: " + t1.level + ", t2.level: " + t2.level);
+    }
+}
