c501aafd36bbf4bbd149a6f77f237da62eabab7c
xuyang-happy <958607524@qq.com>
2019-08-26 10:23:44 +0800

    Exercise 9, page 111

---
+++ src/com/xuyang/thinkinginjava/Operators/MinMax.java
@@ -0,0 +1,19 @@
+package com.xuyang.thinkinginjava.Operators;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/26 10:22
+ * Exercise 9, page 111
+ */
+public class MinMax {
+    public static void main(String[] args) {
+        double max = java.lang.Double.MAX_VALUE;
+        System.out.println("Max double = " + max);
+        double min = java.lang.Double.MIN_VALUE;
+        System.out.println("Min double = " + min);
+        float maxf = java.lang.Float.MAX_VALUE;
+        System.out.println("Max float = " + maxf);
+        float minf = java.lang.Float.MIN_VALUE;
+        System.out.println("Min float = " + minf);
+    }
+}
