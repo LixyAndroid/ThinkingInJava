b0e98ec5e43f83c06fcdc3b97125ad78c3a5337b
xuyang-happy <958607524@qq.com>
2019-08-26 09:52:50 +0800

    第三章 Exercise 1, pager 94

---
+++ src/com/xuyang/thinkinginjava/Operators/PrintTest.java
@@ -0,0 +1,17 @@
+package com.xuyang.thinkinginjava.Operators;
+
+//import static net.mindview.util.Print.*;
+//import org.greggordon.tools.*;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/26 9:49
+ * Exercise 1, pager 94
+ */
+public class PrintTest {
+    public static void main(String[] args) {
+//        print("Hello, from short form.");
+//        P.rintln("Hello from greggordon form.");
+        System.out.println("Hello from long form.");
+    }
+}
