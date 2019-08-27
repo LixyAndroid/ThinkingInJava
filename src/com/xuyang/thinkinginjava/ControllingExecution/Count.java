314eecf7562785607a1964616df01da592c56154
xuyang-happy <958607524@qq.com>
2019-08-27 10:08:54 +0800

    第四章 控制执行流程 Exercise 1, page 139

---
+++ src/com/xuyang/thinkinginjava/ControllingExecution/Count.java
@@ -0,0 +1,13 @@
+package com.xuyang.thinkinginjava.ControllingExecution;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/27 10:07
+ * Exercise 1, page 139
+ */
+public class Count {
+    public static void main(String[] args) {
+        for(int i = 1; i < 101; i++)
+            System.out.println(i);
+    }
+}
