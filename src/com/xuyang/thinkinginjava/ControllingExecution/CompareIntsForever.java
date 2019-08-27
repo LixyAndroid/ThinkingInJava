814833fe8f9b12c6e9d5a08c3e7dc73a8c07aed9
xuyang-happy <958607524@qq.com>
2019-08-27 10:15:43 +0800

    Exercise 3, page 139

---
+++ src/com/xuyang/thinkinginjava/ControllingExecution/CompareIntsForever.java
@@ -0,0 +1,31 @@
+package com.xuyang.thinkinginjava.ControllingExecution;
+
+import java.util.Random;
+
+/**
+ * @author Li Xuyang
+ * date  2019/8/27 10:13
+ * Exercise 3, page 139
+ */
+public class CompareIntsForever {
+    public static void main(String[] args) {
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+        for(int i = 0; i < 25; i++) {
+            int x = rand1.nextInt();
+            int y = rand2.nextInt();
+            if(x < y) System.out.println(x + " < " + y);
+            else if(x > y) System.out.println(x + " > " + y);
+            else System.out.println(x + " = " + y);
+        }
+        Random rand3 = new Random();
+        Random rand4 = new Random();
+        while(true) {
+            int x = rand3.nextInt(10);
+            int y = rand4.nextInt(10);
+            if(x < y) System.out.println(x + " < " + y);
+            else if(x > y) System.out.println(x + " > " + y);
+            else System.out.println(x + " = " + y);
+        }
+    }
+}
