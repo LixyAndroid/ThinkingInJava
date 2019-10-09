4c576ef6f0b42e7dfff7413667009def7e977176
Administrator <958607524@qq.com>
2019-10-09 21:14:06 +0800

    Exercise 4, page 227

---
+++ src/com/xuyang/thinkinginjava/AccessControl/CookieThief.java
@@ -0,0 +1,13 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:13
+ * Exercise 4, page 227
+ */
+public class CookieThief {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        //! x.bite(); // access protected
+    }
+}
