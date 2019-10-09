bd1c25ff2ecf247542efbba187cfbcb03b597358
Administrator <958607524@qq.com>
2019-10-09 21:09:18 +0800

    Exercise 2, page 217

---
+++ src/com/xuyang/thinkinginjava/AccessControl/Collision.java
@@ -0,0 +1,17 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+import net.mindview.simple.*;
+import java.util.*;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:08
+ * Exercise 2, page 217
+ */
+public class Collision {
+    public static void main(String[] args) {
+        // Vector v = new Vector(); // ambiquous collision
+        net.mindview.simple.Vector v1 = new net.mindview.simple.Vector();
+        java.util.Vector v2 = new java.util.Vector();
+    }
+}
