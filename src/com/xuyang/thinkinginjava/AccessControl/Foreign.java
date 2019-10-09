40c612a9cddc8c5d860d4797375f94d72961bc50
Administrator <958607524@qq.com>
2019-10-09 21:26:21 +0800

    Exercise 9, page 233

---
+++ src/com/xuyang/thinkinginjava/AccessControl/Foreign.java
@@ -0,0 +1,12 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:25
+ * Exercise 9, page 233
+ */
+public class Foreign {
+    public static void main(String[] args) {
+        PackagedClass pc = new PackagedClass();
+    }
+}
---
+++ src/com/xuyang/thinkinginjava/AccessControl/PackagedClass.java
@@ -0,0 +1,11 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:25
+ */
+public class PackagedClass {
+    public PackagedClass() {
+		System.out.println("Creating a packaged class");
+	}
+}
