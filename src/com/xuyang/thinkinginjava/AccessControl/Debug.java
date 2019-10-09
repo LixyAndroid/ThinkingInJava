9a9a698bac8963b02b4254dfcac9167c9eaa4d76
Administrator <958607524@qq.com>
2019-10-09 21:11:28 +0800

    Exercise 3, page 220

---
+++ src/com/xuyang/thinkinginjava/AccessControl/Debug.java
@@ -0,0 +1,12 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:10
+ * Exercise 3, page 220
+ */
+public class Debug {
+    public static void debug(String s) {
+        System.out.println(s);
+    }
+}
