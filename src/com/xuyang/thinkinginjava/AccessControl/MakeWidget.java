1a23660ed25b4a7e4d7130bc2437bdea44e9b93b
Administrator <958607524@qq.com>
2019-10-09 21:23:59 +0800

    Exercise 7, page 230

---
+++ src/com/xuyang/thinkinginjava/AccessControl/MakeWidget.java
@@ -0,0 +1,12 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:23
+ * Exercise 7, page 230
+ */
+public class MakeWidget {
+    public static void main(String[] args) {
+        Widget w = new Widget();
+    }
+}
---
+++ src/com/xuyang/thinkinginjava/AccessControl/Widget.java
@@ -0,0 +1,9 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:23
+ */
+public class Widget {
+    public Widget() { System.out.println("Widget()"); }
+}
