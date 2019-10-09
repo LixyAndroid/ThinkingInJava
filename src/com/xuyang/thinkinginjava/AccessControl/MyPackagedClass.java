2a1e1b38005eeed6d140dedf456e0867625d9544
Administrator <958607524@qq.com>
2019-10-09 21:04:26 +0800

    Exercise 1, page 217

---
+++ src/com/xuyang/thinkinginjava/AccessControl/MyPackagedClass.java
@@ -0,0 +1,9 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:01
+ */
+public class MyPackagedClass {
+    public MyPackagedClass() {System.out.println("MyPackagedClass()");}
+}
---
+++ src/com/xuyang/thinkinginjava/AccessControl/UnpackagedMyClass.java
@@ -0,0 +1,12 @@
+package com.xuyang.thinkinginjava.AccessControl;
+
+/**
+ * @author Li Xuyang
+ * @date : 2019/10/9 21:00
+ * Exercise 1, page 217
+ */
+public class UnpackagedMyClass {
+    public static void main(String[] args) {
+        MyPackagedClass m = new MyPackagedClass();
+    }
+}
